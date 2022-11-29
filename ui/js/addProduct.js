9e0d888b784da0b0b6df285bb85d818e9454fa27
daisy-dotcom <61582926+daisy-dotcom@users.noreply.github.com>
2022-11-29 23:22:30 +0300

    added dynamic sub-form and frontend classes

--- ui/js/addProduct.js
+++ ui/js/addProduct.js
@@ -1,4 +1,42 @@
-$(document).ready( function(){
+const subForm = {
+    DVD: `<label for="size">Size (MB)</label> \n
+        <input id="size" type="number" min="0" step="1" name="size" required>`,
+    Book: `<label for="weight">Weight (KG)</label> \n
+        <input id="weight" type="number" min="0" step="1" name="weight" required>`,
+    Furniture: `<label for="height">Height (CM)</label> \n
+    <input id="height" type="number" min="0" step="1" name="height" required> \n
 
+    <label for="width">Width (CM)</label> \n
+    <input id="width" type="number" min="0" step="1" name="width" required> \n
+
+    <label for="length">Length (CM)</label> \n
+    <input id="length" type="number" min="0" step="1" name="length" required>`
+
+};
+
+$(document).ready( function(){
+    $('#productType').change(generateSubForm);
+    $('button').click(buttonEventListener);
 
 });
+
+function generateSubForm(){
+    $('#sub-form').html('')
+    $('#sub-form').append(subForm[$(this).val()]);
+}
+
+function buttonEventListener(){
+    if ($(this).text() === 'SAVE'){
+        saveProduct();
+    }
+    else{
+        cancelProductAdd();
+    }
+}
+
+function saveProduct(){
+    $('#product_form').validate();
+    console.log(validator.errors()); 
+
+
+}
